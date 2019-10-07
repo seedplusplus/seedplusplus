e93f2f6b549f38317212e51c69db7f8fb2280141
Thomas McManus <mcmanustfj@gmail.com>
2019-10-07 17:10:35 -0400

    Added all necessary tables to database

--- lesson/admin.py
+++ lesson/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from lesson.models import Lesson, Tag
+from lesson.models import Lesson, Tag, Comment, Curriculum
 
 class LessonAdmin(admin.ModelAdmin):
     pass
@@ -7,5 +7,15 @@ class LessonAdmin(admin.ModelAdmin):
 class TagAdmin(admin.ModelAdmin):
     pass
 
+class CurriculumAdmin(admin.ModelAdmin):
+    pass
+
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Tag, TagAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Curriculum, CurriculumAdmin)
+
--- lesson/models.py
+++ lesson/models.py
@@ -1,13 +1,70 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+from django.contrib.contenttypes.models import ContentType
 
 
 class Tag(models.Model):
     name = models.CharField(max_length=50)
 
 
-class Lesson(models.Model):
-    title = models.CharField(max_length=255)
-    body = models.TextField()
+class UserCreatedObject(models.Model):
+    class Meta:
+        abstract = True
+
+    owner = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
+
+
+class Comment(UserCreatedObject):
+    body = models.TextField()
+    parent_comment = models.ForeignKey('Comment', related_name='children', on_delete=models.CASCADE)
+    score = models.IntegerField(default=0)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id=models.PositiveIntegerField()
+    content_object = GenericForeignKey()
+
+
+
+class Rating(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    obj = GenericForeignKey('content_type', 'object_id')
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    score = models.PositiveSmallIntegerField(default=0)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id=models.PositiveIntegerField()
+    content_object = GenericForeignKey()
+
+
+
+class CommentableObject(UserCreatedObject):
+    class Meta:
+        abstract = True
+
+    comments = GenericRelation(Comment, related_query_name='comments')
+    ratings = GenericRelation(Rating, related_query_name='ratings')
+    avg_rating = models.FloatField(default=0.0)
+
+
+class Lesson(CommentableObject):
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True)
+    body = models.TextField(null=True)
+    difficulty = models.CharField(max_length=20)
+    language = models.CharField(max_length=30)
+    length = models.CharField(max_length=255)
     tags = models.ManyToManyField('Tag', related_name='lessons')
+
+
+class Curriculum(CommentableObject):
+    body = models.TextField()
+    lessons = models.ManyToManyField('Lesson', related_name='curricula')
+    tags = models.ManyToManyField('Tag', related_name='curricula')
+
+
+class Vote(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    value = models.SmallIntegerField(default=0)
