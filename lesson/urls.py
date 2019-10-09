83ee979c51642390d78f999d233d42b7991c1890
Jonathan Harris <harrij15@rpi.edu>
2019-10-09 17:46:15 -0400

    Implemented Delete

--- lesson/urls.py
+++ lesson/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("lessons/<int:pk>/", views.lesson_detail, name="lesson_detail"),
     path("lessons/<int:pk>/edit/", views.lesson_edit, name="lesson_edit"),
     path("tags/<tag>/", views.lesson_tag, name="lesson_tag"),
-    path("lessons/new/", views.lesson_new, name="lesson_new")
+    path("lessons/new/", views.lesson_new, name="lesson_new"),
+    path("lessons/<int:pk>/delete",views.lesson_delete,name="lesson_delete")
 ]
