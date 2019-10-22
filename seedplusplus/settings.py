0687077793875c0d65831841e8c6b45a7474c4ff
Thomas McManus <mcmanustfj@gmail.com>
2019-10-22 11:46:35 -0400

    Added login/logout functionality and new 'registration' app

--- seedplusplus/settings.py
+++ seedplusplus/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lesson',
-    'compressor'
+    'registration',
+    'compressor',
 ]
 
 MIDDLEWARE = [
@@ -139,3 +140,5 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     'compressor.finders.CompressorFinder',
 )
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
