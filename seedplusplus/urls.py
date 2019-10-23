041c3f6643700fb49380306fecc9855f5147ec1c
Jonathan Harris <harrij15@rpi.edu>
2019-10-22 20:25:08 -0400

    Added Sign Up and Log In buttons

--- seedplusplus/urls.py
+++ seedplusplus/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('accounts/', include('registration.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path("", include("lesson.urls")),
+    path("", include("registration.urls")),
 ]
