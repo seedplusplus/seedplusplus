997820e94b1ffa754c2c3de99d42e1462aabbbdb
dsp125 <dpatel0125@gmail.com>
2019-11-24 17:10:22 -0500

    :(

--- seedplusplus/settings.py
+++ seedplusplus/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
       'ENGINE': 'django.db.backends.postgresql',
       'NAME': 'SEED++',
       'USER': 'postgres',
-        'PASSWORD': 'admin',
+        'PASSWORD': 'postgres',
       'HOST': 'localhost',
       'PORT': '',
     }
